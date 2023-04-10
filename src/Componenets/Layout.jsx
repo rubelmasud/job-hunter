0120daa559e11a1c50146cfce705eaa2c1f19b79
MD:Rubrl RM <inforubel251@gmail.com>
2023-04-10 16:06:09 +0600

    dausi ui install and create footer

--- src/Componenets/Layout.jsx
+++ src/Componenets/Layout.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Header from './Header';
 import { Outlet } from 'react-router-dom';
+import Footer from './Footer';
 
 const Layout = () => {
     return (
         <>
             <Header />
-            <Outlet />
+            <div className="md:min-h-[calc(100vh-100px)]">
+                <Outlet />
+            </div>
+            <Footer />
         </>
     );
 };
